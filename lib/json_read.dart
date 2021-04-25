01800d11c3c52af404f9cd72d603332f5357f3a0
HAFDIAHMED <ahafdi62@gmail.com>
2021-04-25 23:18:02 +0200

    changes

--- lib/json_read.dart
+++ lib/json_read.dart
@@ -41,37 +41,9 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          'Dashboard',
+          'Kindacode.com',
         ),
       ),
-      drawer: Drawer(
-  child: ListView(
-    // Important: Remove any padding from the ListView.
-    padding: EdgeInsets.zero,
-    children: <Widget>[
-      DrawerHeader(
-        child: Text('Drawer Header'),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-        ),
-      ),
-      ListTile(
-        title: Text('Item 1'),
-        onTap: () {
-          
-        },
-      ),
-      ListTile(
-        title: Text('Item 2'),
-        onTap: () {
-        
-        },
-      ),
-    ],
-  ),
-),
-
-
       body: Padding(
         padding: const EdgeInsets.all(25),
         child: Column(
